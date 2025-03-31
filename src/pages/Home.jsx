b15ee60a424c1802c011033b8afb98bc1a84f3d8
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-03-31 21:18:49 +0300

    feat(Home): Создать ссылку на авторизацию.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,25 @@
+import { Link } from 'react-router'
+
 import { useAuth } from '../context'
 
 export function Home() {
   const auth = useAuth()
 
   return (
-    <h1>
-      Добро пожаловать
-      <span style={{ color: '#65e665' }}>
-        {' '}
-        {auth.user && auth.user.email}
-      </span>{' '}
-      на страничку о вселенной Рика и Морти!
-    </h1>
+    <>
+      <h1>
+        Добро пожаловать
+        <span style={{ color: '#65e665' }}>
+          {' '}
+          {auth.user && auth.user.email}
+        </span>{' '}
+        на страничку о вселенной Рика и Морти!
+      </h1>
+      {auth.user === null && (
+        <p>
+          Давай познакомимся, <Link to="/signin">войди</Link> в аккаунт.
+        </p>
+      )}
+    </>
   )
 }
